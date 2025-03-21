64f8dc5e349654836cb5f1deef87087531b36749
murali krishna <gannojumuralikrishna@gmail.com>
2025-03-21 17:58:15 +0530

    Updated workflow

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, FormsModule],
+    declarations: [AppComponent, CalculatorComponent]
   }));
 
   it('should create the app', () => {
@@ -20,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('script');
   });
 
-  it('should render title', () => {
+  it('should render calculator component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('script app is running!');
+    expect(compiled.querySelector('app-calculator')).toBeTruthy();
   });
 });
